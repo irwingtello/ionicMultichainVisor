70b67d50e6293009da2db04276ff737cfe774533
samuelpolino97 <polinosamuel1997@outlook.com>
2022-09-23 01:39:13 -0500

    a select was added on the screen offline

--- src/pages/Offline.tsx
+++ src/pages/Offline.tsx
@@ -1,6 +1,20 @@
-import { IonContent, IonGrid, IonHeader, IonPage, IonTitle, IonToolbar, IonRow,IonCol,IonButton, IonList, IonItem, IonSelect, IonSelectOption } from '@ionic/react';
+import {
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonList,
+  IonItem,
+  IonSelect,
+  IonSelectOption,
+} from "@ionic/react";
 
-import './About.css';
+import "./About.css";
 
 const Offline: React.FC = () => {
   return (
@@ -16,38 +30,53 @@ const Offline: React.FC = () => {
             <IonTitle size="large">Offline</IonTitle>
           </IonToolbar>
         </IonHeader>
-      <IonGrid>
-
-      <IonRow class="row-class">
-            <IonCol class="cell-class">
-              <div className="text-lg ion-margin-bottom ion-text-justify">
-              <IonList>
-      <IonItem  style={{ fontSize: "15px" }} >
-        <IonSelect interface="popover" placeholder="Select NFT">
-          <IonSelectOption value="ETH">ETH</IonSelectOption>
-          <IonSelectOption value="Polygon">Polygon</IonSelectOption>
-          <IonSelectOption value="BNB">BNB</IonSelectOption>
-          <IonSelectOption value="Fantom">Fantom</IonSelectOption>
-          <IonSelectOption value="Avalanche">Avalanche</IonSelectOption>
-          <IonSelectOption value="POAP">POAP</IonSelectOption>
-        </IonSelect>
-      </IonItem>
-    </IonList>
+        <IonGrid>
+          <IonRow class="row-class">
+            <IonCol size="12">
+              <IonSelect
+                mode="ios"
+                className="ion-hide-lg-up"
+                interface="action-sheet"
+                placeholder="Select NFT"
+              >
+                <IonSelectOption value="ETH">ETH</IonSelectOption>
+                <IonSelectOption value="Polygon">Polygon</IonSelectOption>
+                <IonSelectOption value="BNB">BNB</IonSelectOption>
+                <IonSelectOption value="Fantom">Fantom</IonSelectOption>
+                <IonSelectOption value="Avalanche">Avalanche</IonSelectOption>
+                <IonSelectOption value="POAP">POAP</IonSelectOption>
+              </IonSelect>
+              <div className="select">
+                <select
+                  className="ion-hide-lg-down"
+                  name="NFTS"
+                  id="lang"
+                  style={{ width: "604px", height: "64px", margin: "5 auto" }}
+                >
+                  <option value="ETH1">ETH</option>
+                  <option value="Polygon1">Polygon</option>
+                  <option value="BNB1">BNB</option>
+                  <option value="Fantom1">Fantom</option>
+                  <option value="Avalanche1">Avalanche</option>
+                  <option value="POAP1">POAP</option>
+                </select>
               </div>
+            </IonCol>
+            <IonCol>
+              <IonButton
+                color="primary"
+                className="ion-activatable ripple-parent"
+              >
+                Search
+              </IonButton>
             </IonCol>
           </IonRow>
 
           <IonRow>
             {/*Button Search */}
-            <IonCol class="cell-class cell-align cell-buttons-size ">
-              <IonButton color="primary" className="ion-activatable ripple-parent" style={{}}>
-                Search
-              </IonButton>
-
-              </IonCol>
-         </IonRow>
-
-      </IonGrid>
+            <IonCol class="cell-class cell-align cell-buttons-size "></IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
